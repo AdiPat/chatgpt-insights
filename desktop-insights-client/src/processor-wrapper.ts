78c69695ef4f5199d65a1f46ce0a373e890febdb
Aditya Patange <contact.adityapatange@gmail.com>
2024-12-22 05:42:26 +0530

    build fixes and import updates

--- desktop-insights-client/src/processor-wrapper.ts
+++
@@ -1,8 +0,0 @@
-async function processInsightFile(filepath: string) {
-  const { processZipFile, InsightEngine } = await import("chatgpt-insights");
-  const conversations = await processZipFile(filepath, "dummy-key");
-  const engine = new InsightEngine(conversations);
-  return engine.generateReport();
-}
-
-export const processFile = processInsightFile;
